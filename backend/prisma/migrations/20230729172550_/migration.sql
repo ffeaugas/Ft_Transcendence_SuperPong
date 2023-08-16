10451d405b01aaf97bef89f50919403ab49f1794
jlarrieu <joris.larrieux@gmail.com>
2023-08-16 11:33:26 +0200

    merged migrations db prisma

--- backend/prisma/migrations/20230729172550_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "profiles" ALTER COLUMN "profilePicture" SET DEFAULT 'https://stickerly.pstatic.net/sticker_pack/V3UsPH6pEtk8qTcLjMqdQ/F4MF00/33/0a5a27ca-3840-4383-8431-918ae45ff2ef.png';
