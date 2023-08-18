2b2bc9da36de5fa0bd48078e9d942d433fc83323
franci <franci@franci.42.fr>
2023-08-18 18:07:42 +0200

    cleaning some stuff

--- frontend/src/app/achievements/page.tsx
+++ frontend/src/app/achievements/page.tsx
@@ -11,25 +11,25 @@ type Achievement = {
   picture: string;
 };
 
-async function getAchievements(): Promise<Achievement[] | undefined> {
-  const res = await fetch("http://10.5.0.3:3001/achievement/all");
-  if (!res.ok) {
-    console.log(res);
-    throw new Error("Failed fetching achievements");
-  }
-  const response = await res.json();
-  return response;
-}
-
 export default async function Achievements() {
   const achievements: Achievement[] | undefined = await getAchievements();
+
+  async function getAchievements(): Promise<Achievement[] | undefined> {
+    const res = await fetch("http://10.5.0.3:3001/achievement/all");
+    if (!res.ok) {
+      console.log(res);
+      throw new Error("Failed fetching achievements");
+    }
+    const response = await res.json();
+    return response;
+  }
 
   if (!achievements) {
     return <p>prout</p>;
   }
 
   return (
-    <section className={`${styles.page}`}>
+    <section className={styles.page}>
       <Header />
       <h1>Achievements</h1>
       <div className={styles.achievements}>
--- frontend/src/components/Chat/AdministrateChannel.tsx
+++ frontend/src/components/Chat/AdministrateChannel.tsx
@@ -5,149 +5,126 @@ import styles from "../../styles/Chat/AdministrateChannel.module.css";
 import axios from "axios";
 
 enum ChannelMode {
-    PRIVATE = "PRIVATE",
-    PUBLIC = "PUBLIC",
-    PROTECTED = "PROTECTED",
+  PRIVATE = "PRIVATE",
+  PUBLIC = "PUBLIC",
+  PROTECTED = "PROTECTED",
 }
 
 type ChannelInfos = {
-    channelName: string;
-    password?: string;
-    mode: ChannelMode;
+  channelName: string;
+  password?: string;
+  mode: ChannelMode;
 };
 
 type AdministrateChannelProps = {
-    activeChannel: string;
+  activeChannel: string;
 };
 
 export default function AdministrateChannel({
-    activeChannel,
+  activeChannel,
 }: AdministrateChannelProps) {
-    const [channelInfos, setChannelInfos] = useState<ChannelInfos>({
-        channelName: "",
-        password: "",
-        mode: ChannelMode.PUBLIC,
-    });
-    const [feedbackMessage, setFeedbackMessage] = useState<string>("");
+  const [channelInfos, setChannelInfos] = useState<ChannelInfos>({
+    channelName: "",
+    password: "",
+    mode: ChannelMode.PUBLIC,
+  });
+  const [feedbackMessage, setFeedbackMessage] = useState<string>("");
 
-    // async function deleteChannel(): Promise<void> {
-    //   try {
-    //     const res = await axios.delete("http://10.5.0.3:3001/channels/delete", {
-    //       headers: {
-    //         Authorization: "Bearer " + localStorage.getItem("token"),
-    //       },
-    //       data: {
-    //         channelName: activeChannel,
-    //       },
-    //     });
-    //     if (res.status) {
-    //       const channelName = channelInfos.channelName;
-    //       setFeedbackMessage("Channel update success!");
-    //     }
-    //   } catch (error: any) {
-    //     console.log(error.message);
-    //   }
-    // }
-
-    async function deleteChannel() {
-        try {
-            const response = await fetch(
-                `http://10.5.0.3:3001/channels/${activeChannel}`,
-                {
-                    method: "DELETE",
-                    headers: {
-                        Authorization:
-                            "Bearer " + localStorage.getItem("token"),
-                    },
-                }
-            );
-            if (response.ok) {
-                setFeedbackMessage("Channel successfully deleted!");
-            } else {
-                setFeedbackMessage(
-                    "Problem occured when trying to delete channel!"
-                );
-            }
-        } catch (error) {
-            setFeedbackMessage("You can't delete this channel!");
-            console.error(error);
+  async function deleteChannel() {
+    try {
+      const response = await fetch(
+        `http://10.5.0.3:3001/channels/${activeChannel}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
         }
+      );
+      if (response.ok) {
+        setFeedbackMessage("Channel successfully deleted!");
+      } else {
+        setFeedbackMessage("Problem occured when trying to delete channel!");
+      }
+    } catch (error) {
+      setFeedbackMessage("You can't delete this channel!");
+      console.error(error);
     }
+  }
 
-    function handleChange(evt: any): void {
-        const { name, value } = evt.target;
-        setChannelInfos({ ...channelInfos, [name]: value });
-    }
+  function handleChange(evt: any): void {
+    const { name, value } = evt.target;
+    setChannelInfos({ ...channelInfos, [name]: value });
+  }
 
-    async function handleSubmit(evt: any): Promise<void> {
-        evt.preventDefault();
-        try {
-            const res = await axios.patch(
-                "http://10.5.0.3:3001/channels/change-mode",
-                {
-                    channelName: activeChannel,
-                    mode: channelInfos.mode,
-                    password: "",
-                },
-                {
-                    headers: {
-                        Authorization:
-                            "Bearer " + localStorage.getItem("token"),
-                    },
-                }
-            );
-            if (res.status) {
-                const channelName = channelInfos.channelName;
-                setChannelInfos({
-                    channelName: "",
-                    password: "",
-                    mode: ChannelMode.PUBLIC,
-                });
-                setFeedbackMessage("Channel update success!");
-            } else {
-                setFeedbackMessage("Channel update failed.");
-            }
-        } catch (error: any) {
-            console.log(error.message);
+  async function handleSubmit(evt: any): Promise<void> {
+    evt.preventDefault();
+    try {
+      const res = await axios.patch(
+        "http://10.5.0.3:3001/channels/change-mode",
+        {
+          channelName: activeChannel,
+          mode: channelInfos.mode,
+          password: "",
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
         }
+      );
+      if (res.status) {
+        const channelName = channelInfos.channelName;
+        setChannelInfos({
+          channelName: "",
+          password: "",
+          mode: ChannelMode.PUBLIC,
+        });
+        setFeedbackMessage("Channel update success!");
+      } else {
+        setFeedbackMessage("Channel update failed.");
+      }
+    } catch (error: any) {
+      console.log(error.message);
     }
+  }
 
-    return (
-        <div className={styles.adminChannel}>
-            <h2>
-                Manage Channel<br></br>
-                <b>{activeChannel}</b>
-            </h2>
-            <form onSubmit={(evt) => handleSubmit(evt)}>
-                <div className={styles.subform}>
-                    <label htmlFor="channelmode">Change mode :</label>
-                    <select
-                        name="mode"
-                        id="channelmode"
-                        value={channelInfos.mode}
-                        onChange={(evt) => handleChange(evt)}
-                    >
-                        <option value="PUBLIC">Public</option>
-                        <option value="PRIVATE">Private</option>
-                        <option value="PROTECTED">Protected</option>
-                    </select>
-                </div>
-                {channelInfos.mode === ChannelMode.PROTECTED ? (
-                    <div className={styles.subform}>
-                        <label htmlFor="password">Change password :</label>
-                        <input
-                            type="text"
-                            name="password"
-                            id="password"
-                            value={channelInfos.password}
-                            onChange={(evt) => handleChange(evt)}
-                        />
-                    </div>
-                ) : undefined}
-                <input type="submit" value="Update" />
-            </form>
-            <button onClick={deleteChannel}>Delete</button>
-            <p>{feedbackMessage}</p>
+  return (
+    <div className={styles.adminChannel}>
+      <h2>
+        Manage Channel<br></br>
+        <b>{activeChannel}</b>
+      </h2>
+      <form onSubmit={(evt) => handleSubmit(evt)}>
+        <div className={styles.subform}>
+          <label htmlFor="channelmode">Change mode :</label>
+          <select
+            name="mode"
+            id="channelmode"
+            value={channelInfos.mode}
+            onChange={(evt) => handleChange(evt)}
+          >
+            <option value="PUBLIC">Public</option>
+            <option value="PRIVATE">Private</option>
+            <option value="PROTECTED">Protected</option>
+          </select>
         </div>
-    );
+        {channelInfos.mode === ChannelMode.PROTECTED ? (
+          <div className={styles.subform}>
+            <label htmlFor="password">Change password :</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              value={channelInfos.password}
+              onChange={(evt) => handleChange(evt)}
+            />
+          </div>
+        ) : undefined}
+        <input type="submit" value="Update" />
+      </form>
+      <button onClick={deleteChannel}>Delete</button>
+      <p>{feedbackMessage}</p>
+    </div>
+  );
 }
