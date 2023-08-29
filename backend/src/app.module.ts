7718d4e82dcc6ab862e6e72db995f0731402be52
jlarrieu <joris.larrieux@gmail.com>
2023-08-29 17:54:05 +0200

    impl game in the website

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { monitor } from '@colyseus/monitor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -17,6 +18,7 @@ import { AchievementController } from './achievement/achievement.controller';
 import { AchievementService } from './achievement/achievement.service';
 import { AchievementModule } from './achievement/achievement.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { GameService } from './game.service';
 
 @Module({
   imports: [
@@ -38,6 +40,11 @@ import { LeaderboardModule } from './leaderboard/leaderboard.module';
     UsersService,
     ProfileService,
     AchievementService,
+    GameService,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(monitor()).forRoutes('/monitor');
+  }
+}
--- frontend/src/app/game/page.tsx
+++ frontend/src/app/game/page.tsx
@@ -3,11 +3,40 @@
 import Header from "@/components/Header";
 import styles from "../../styles/page.module.css";
 
-export default function Game() {
-  return (
-    <section className={`${styles.page}`}>
-      <Header />
-      <h1>GAME</h1>
-    </section>
-  );
-}
+// export default function Game() {
+//   return (
+//     <section className={`${styles.page}`}>
+//       <Header />
+//       <h1>GAME</h1>
+//     </section>
+//   );
+// }
+
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+    () => import("@/components/game/index"),
+    {
+        ssr: false,
+    }
+);
+
+const Game = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+    }, []);
+
+    return (
+        <section className={`${styles.page}`}>
+            <Header />
+            <h1>GAME</h1>
+            <div key={Math.random()} id="game"></div>
+            {loading ? <DynamicComponentWithNoSSR /> : null}
+        </section>
+    );
+};
+
+export default Game;
