c4ae23dcc2b0e1601b2852eb94e302517c8d0f77
jlarrieu <joris.larrieux@gmail.com>
2023-09-06 13:04:58 +0200

    oui

--- frontend/src/components/game/index.ts
+++ frontend/src/components/game/index.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import MainMenuScene from "./scenes/menu";
 import GameScene from "./scenes/main";
-=======
-import Main from "./main";
->>>>>>> impl_game
 import { useEffect } from "react";
 import "phaser";
 
@@ -16,7 +12,6 @@ export default function Index() {
         if (typeof window !== "object") {
             return;
         }
-<<<<<<< HEAD
     };
     const config: Phaser.Types.Core.GameConfig = {
         type: Phaser.AUTO,
@@ -41,42 +36,4 @@ export default function Index() {
         pixelArt: true,
         scene: [MainMenuScene, GameScene],
     };
-
-    // instantiate the game
-    const game = new Phaser.Game(config);
-=======
-
-        const config: Phaser.Types.Core.GameConfig = {
-            type: Phaser.AUTO,
-            scale: {
-                mode: Phaser.Scale.RESIZE,
-                // mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
-                autoCenter: Phaser.Scale.CENTER_BOTH,
-            },
-            backgroundColor: "#000000",
-            parent: "phaser-example",
-            physics: {
-                default: "arcade",
-                arcade: {
-                    debug: true,
-                },
-            },
-            fps: {
-                target: 60,
-                min: 60,
-                forceSetTimeOut: true,
-            },
-            pixelArt: true,
-            scene: [Main],
-        };
-
-        // instantiate the game
-        const game = new Phaser.Game(config);
-
-        // game.scene.add("main", Main);
-        // game.scene.start("main");
-    };
-
-    return null;
->>>>>>> impl_game
 }
