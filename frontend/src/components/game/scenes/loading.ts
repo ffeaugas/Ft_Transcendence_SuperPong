bb7f6bd4cdbf04c81b8d000d9833fa3aca3c99ed
ssubielo <subielos.samuel@gmail.com>
2023-09-09 05:50:28 +0200

    player name add but bof/20

--- frontend/src/components/game/scenes/loading.ts
+++ frontend/src/components/game/scenes/loading.ts
@@ -10,6 +10,8 @@ export default class LoadingScene extends Phaser.Scene {
     nb_client: number = 0;
     loadingEntities: { [index: number]: any } = {};
     backgroundEntities: { [index: number]: any } = {};
+    nameEntities: { [index: number]: any } = {};
+    name: { [index: number]: any } = {};
     colorb = 0x0d0721;
     colorr = 0x00bce1;
     nbPlayer: number = 0;
@@ -34,7 +36,7 @@ export default class LoadingScene extends Phaser.Scene {
         const user = await res.json();
         return user["username"];
     }
-    async getPP(): Promise<string> {
+    /* async getPP(): Promise<string> {
         const res = await fetch(
             `http://${process.env.NEXT_PUBLIC_DOMAIN}:3001/users/me`,
             {
@@ -46,7 +48,7 @@ export default class LoadingScene extends Phaser.Scene {
         );
         const user = await res.json();
         return user["profile/profilePicture"];
-    }
+    }*/
 
     async create() {
         console.log("Joining room...");
@@ -58,20 +60,53 @@ export default class LoadingScene extends Phaser.Scene {
             this.scale.refresh();
             this.backgroundEntities[0] = this.add
                 .rectangle(dim[0] / 2, dim[1] / 2, 20, 5000, 0x111111)
-                .setDepth(-2);
+                .setDepth(5);
             this.backgroundEntities[3] = this.add
                 .circle((dim[0] / 4) * 3, dim[1] / 3, 100, 0x111111)
                 .setDepth(5);
             this.backgroundEntities[4] = this.add
                 .circle(dim[0] / 4, dim[1] / 3, 100, 0x111111)
                 .setDepth(5);
+            this.backgroundEntities[5] = this.add
+                .rectangle(
+                    (dim[0] / 4) * 3,
+                    (dim[1] / 3) * 2,
+                    400,
+                    50,
+                    0x111111
+                )
+                .setDepth(5);
+            //this.backgroundEntities[5].setCrop(0.5);
+            this.backgroundEntities[6] = this.add
+                .rectangle(dim[0] / 4, (dim[1] / 3) * 2, 400, 50, 0x111111)
+                .setDepth(5);
+            this.nameEntities[0] = this.add
+                .text(dim[0] / 4, (dim[1] / 3) * 2, "", {
+                    fontFamily:
+                        'Georgia, "Goudy Bookletter 1911", Times, serif',
+                    fontSize: 35,
+                    align: "center",
+                    color: "#666666",
+                })
+                .setOrigin(0.5)
+                .setDepth(6);
+            this.nameEntities[1] = this.add
+                .text((dim[0] / 4) * 3, (dim[1] / 3) * 2, "", {
+                    fontFamily:
+                        'Georgia, "Goudy Bookletter 1911", Times, serif',
+                    fontSize: 35,
+                    align: "center",
+                    color: "#666666",
+                })
+                .setOrigin(0.5)
+                .setDepth(6);
             this.client = new Client(
                 `ws://${process.env.NEXT_PUBLIC_DOMAIN}:3001`
             );
             this.room = await this.client.joinOrCreate("MyRoom", {
                 dim,
                 name: await this.getUsername(),
-                PpUrl: await this.getPP(),
+                //PpUrl: await this.getPP(),
             }); // this.matchMaker.joinOrCreate("pong", {mode: "classic"});
             // this.room = await this.client.; // this.matchMaker.joinOrCreate("pong", {mode: "classic"});
             this.game.canvas.style.cursor = "none";
@@ -86,38 +121,99 @@ export default class LoadingScene extends Phaser.Scene {
             this.room.onMessage("Joined", (player) => {
                 // this.player = players.username;
                 console.log(player);
+                if (!this.name[0]) {
+                    this.name[0] = player;
+                    this.nameEntities[0].setText(player);
+                } else if (!this.name[1] && this.name[0] != player) {
+                    this.name[1] = player;
+                    this.nameEntities[1].setText(player);
+                }
+                this.client.name = player;
             });
             if (this.nb_client === 2) {
                 setTimeout(() => this.scene.start("Game", this.room), 20000);
             } else if (this.nb_client == 1) {
                 if (sessionId === this.room.sessionId) {
                     this.backgroundEntities[1] = this.add.pointlight(
-                        dim[0],
-                        dim[1] / 2,
-                        0x1a0a10,
-                        800
+                        (dim[0] / 4) * 3,
+                        dim[1] / 3,
+                        0xff0000,
+                        200,
+                        0.5
                     );
                     this.backgroundEntities[2] = this.add.pointlight(
-                        0,
-                        dim[1] / 2,
-                        0x001a10,
-                        800
+                        dim[0] / 4,
+                        dim[1] / 3,
+                        0x00ff00,
+                        200,
+                        0.5
                     );
+                    for (let i = 7; i < 18; i++) {
+                        this.backgroundEntities[i] = this.add.pointlight(
+                            (dim[0] / 4) * 3 - 200 + (i - 7) * 40,
+                            (dim[1] / 3) * 2,
+                            0xaa0000,
+                            70,
+                            0.2
+                        );
+                    }
+                    for (let i = 18; i < 29; i++) {
+                        this.backgroundEntities[i] = this.add.pointlight(
+                            dim[0] / 4 - 200 + (i - 18) * 40,
+                            (dim[1] / 3) * 2,
+                            0x00aa00,
+                            70,
+                            0.2
+                        );
+                    }
                 } else {
-                    this.backgroundEntities[2] = this.add.pointlight(
-                        dim[0],
-                        dim[1] / 2,
-                        0x001a10,
-                        800
-                    );
                     this.backgroundEntities[1] = this.add.pointlight(
-                        0,
-                        dim[1] / 2,
-                        0x1a0a10,
-                        800
+                        (dim[0] / 4) * 3,
+                        dim[1] / 3,
+                        0x00ff00,
+                        200,
+                        0.5
                     );
+                    this.backgroundEntities[2] = this.add.pointlight(
+                        dim[0] / 4,
+                        dim[1] / 3,
+                        0xff0000,
+                        200,
+                        0.5
+                    );
+                    for (let i = 7; i < 18; i++) {
+                        this.backgroundEntities[i] = this.add.pointlight(
+                            (dim[0] / 4) * 3 - 200 + (i - 7) * 40,
+                            (dim[1] / 3) * 2,
+                            0x00aa00,
+                            70,
+                            0.2
+                        );
+                    }
+                    for (let i = 18; i < 29; i++) {
+                        this.backgroundEntities[i] = this.add.pointlight(
+                            dim[0] / 4 - 200 + (i - 18) * 40,
+                            (dim[1] / 3) * 2,
+                            0xaa0000,
+                            70,
+                            0.2
+                        );
+                    }
                 }
             }
+        });
+        this.room.state.players.onRemove((player, sessionId) => {
+            this.nb_client--;
+            console.log(this.client.name);
+            /*let dim = [this.game.canvas.width, this.game.canvas.height];
+            if (this.client.name != this.name[0]) {
+                this.name[0] = "";
+                this.nameEntities[0].setText("");
+            } else if (this.name[1]) {
+                this.name[1] = "";
+                this.nameEntities[1].setText("");
+            }*/
+            // marche pas de ouf refonte avec sessionId a prevoir
         });
         this.events.on("destroy", () => {
             this.room.leave();
