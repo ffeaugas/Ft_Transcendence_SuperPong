cc41c4f2836991c41aafda8d84a585b476f20ed9
ffeaugas <feaugas.francis@gmail.com>
2023-09-22 15:17:11 -0400

    adding the prisma migrations

---
+++ backend/prisma/migrations/20230922140218_updating_user_status/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "status",
+ADD COLUMN     "lastConnexionPing" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
